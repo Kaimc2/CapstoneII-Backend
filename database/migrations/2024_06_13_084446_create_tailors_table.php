d27ca5b0e17fb7f1d1638e0f8d181a4fafe86d6f
KAKADACHHIM <chhimkakada2003@gmail.com>
2024-06-13 16:05:24 +0700

    add migration table: tailors, designs, commissions, and adjustments

---
+++ database/migrations/2024_06_13_084446_create_tailors_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tailors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('description')->unique();
+            $table->decimal('price');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->bigInteger('storeId');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tailors');
+    }
+};
