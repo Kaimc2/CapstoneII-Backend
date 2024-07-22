fd7793644e7d59632ebeff17004555f037873fb3
Hul-Marady <marady.hul@student.cadt.edu.kh>
2024-07-22 15:08:14 +0700

    update UserController

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use App\Http\Controllers\UserController;
 Route::prefix('auth')->group(function () {
     Route::post('register', [AuthController::class, 'register'])->name('register');
     Route::post('login', [AuthController::class, 'login'])->name('login');
-    Route::post('logout', [AuthController::class, 'logout'])->name('register');
+    Route::post('logout', [AuthController::class, 'logout'])->name('logout');
     Route::post('refresh-token', [AuthController::class, 'refresh']);
 
     // Email Verification routes
@@ -38,5 +38,6 @@ Route::group(['middleware' => ['jwt.auth', 'verified']], function () {
     Route::apiResource('permissions', PermissionController::class);
     Route::apiResource('commissions', CommissionController::class);
     Route::apiResource('adjustments', AdjustmentController::class);
+    Route::get('display/{id}', [UserController::class, 'display'])->name('profile');
     Route::apiResource('users', UserController::class);
 });
